e9ca440d4eede52c026b6d62fa2fc6b4dbea5a6e
Michael Favaretto <michael.favaretto@heig-vd.ch>
2011-12-22 12:06:04 +0100

    include file invader.h and invader.c

---
+++ rt-app/invaders.h
@@ -0,0 +1,20 @@
+/*
+ * enemies.h
+ *
+ *  Created on: Dec 22, 2011
+ *      Author: michael
+ */
+
+#ifndef INVADERS_H_
+#define INVADERS_H_
+
+ void init_invaders (Wave *listEnemies );
+
+
+
+
+
+
+
+
+#endif /*ENEMIES_H_*/
